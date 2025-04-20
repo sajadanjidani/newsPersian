ec455c47a7337ed1267e23bc242203071826f028
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-20 22:25:06 +0330

    coding : Navbar component

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -3,7 +3,17 @@ import styles from './navbar.module.css'
 const Navbar = () => {
     return(
         <div className={styles.container}>
-
+            <div className={styles.contentContainer}>
+                <div className={styles.iconbox}></div>
+                <div className={styles.imagebox}></div>
+                <div className={styles.listbox}>
+                    <ul>
+                        <li>بازی</li>
+                        <li>فوتبال</li>
+                        <li>فیلم و سریال</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
